05e5a04ad7ede0d9e8b484683423c402f8769d22
Worrasit Makhasit <64070098@it.kmitl.ac.th>
2022-10-25 18:56:25 +0700

    add text test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <h1>Hello World</h1>
     </div>
   );
 }
