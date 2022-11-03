b478fd86c3f49c4d6639a79d20c7d120a0b5e3aa
Worrasit Makhasit <64070098@it.kmitl.ac.th>
2022-11-03 23:00:07 +0700

    carousel test but failed

--- src/page/carousel/index.js
+++ src/page/carousel/index.js
@@ -3,7 +3,7 @@ import './carousel.css';
 function Carousel() {
     return (
         <div className='max-w-full'>
-            <div className='carouselFrame'>
+            <div>
                 <iframe width='100%' src="https://www.youtube.com/embed/4wKU9oIXnTI?version=3&autoplay=1&loop=1&controls=0&disablekb=1&playlist=4wKU9oIXnTI" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
             </div>
             <div className='floatin'>
