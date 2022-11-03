e8f4c2b5888a61fa4ffe341f061fb165bb1a3c67
Worrasit Makhasit <64070098@it.kmitl.ac.th>
2022-11-03 20:38:05 +0700

    create container in home

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -6,7 +6,12 @@ function Home() {
     return (
         <div>
             <section>
-                <Carousel/>
+                <Carousel />
+                <div className='container mx-auto py-32'>
+                    <h3>iPhone</h3>
+                    <h3>iPad</h3>
+                    <h3>Accessories</h3>
+                </div>
             </section>
         </div>
     );
