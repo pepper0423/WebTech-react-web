ae6806b59171b87e145ecfbddb267dadf6863b3b
JJJurarat <89636615+JJJurarat@users.noreply.github.com>
2022-11-05 17:19:42 +0700

    Iphone14pro

---
+++ src/pages/Iphone14Pro/Compare.js
@@ -0,0 +1,238 @@
+import React from 'react';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+
+const Iphone14ProCompare = () => {
+  return (
+    <div className="my-20">
+      <h2 className="text-4xl font-semibold text-center">
+       iPhone รุ่นไหนเหมาะกับคุณ
+      </h2>
+      <div className="grid grid-cols-2 md:grid-cols-3 text-center mt-12">
+        <div className="p-3 border-r">
+          <div>
+            <img
+              alt="iPhone 13 Pro"
+              className="mx-auto mb-5"
+              src="assets/compareProduct/iphone/iphone-compare-iphone-13-pro.jpg"
+            />
+            <p className="text-sm text-red-600">ขายดีในปี 2021</p>
+            <h6 className="text-2xl font-medium">iPhone 13 Pro</h6>
+            <p className="mt-2 text-lg font-medium">โปรยิ่งกว่าโปร</p>
+            <p className="mt-1 text-lg font-medium">เริ่มต้นที่ ฿38,900</p>
+            <img
+              alt="iPhone 13 Pro"
+              className="mx-auto mt-5"
+              src="assets/compareProduct/iphone/iphone-compare-iphone-13-pro-swatch.jpg"
+            />
+          </div>
+          <div className="my-10">
+            <p className="text-2xl font-medium">6.7″ หรือ 6.1″</p>
+            <p className="mt-1 font-semibold">
+              จอภาพ OLED ทั้งหน้าจอ¹ พร้อม ProMotion
+            </p>
+          </div>
+          <div className="my-10">
+            <img
+              alt="5G"
+              className="mx-auto"
+              src="assets/compareProduct/iphone/iphone-compare-icon-5g.jpg"
+            />
+            <p className="mt-4 font-semibold">5G ที่เร็วสุดแรง³</p>
+          </div>
+          <div className="my-10">
+            <img
+              alt="5G"
+              className="mx-auto"
+              src="assets/compareProduct/iphone/iphone-compare-icon-a15-chip.jpg"
+            />
+            <p className="mt-4 font-semibold">ชิพ A15 Bionic</p>
+            <p className="font-semibold text-gray-400">
+              ชิปที่เร็วที่สุดในสมาร์ทโฟน
+            </p>
+          </div>
+          <div className="my-10">
+            <img
+              alt="iPhone 13 Pro Camera"
+              className="mx-auto"
+              src="assets/compareProduct/iphone/iphone-compare-icon-iphone-13-pro-camera.jpg"
+            />
+            <p className="mt-4 font-semibold">ระบบกล้องระดับโปร</p>
+            <p className="font-semibold text-gray-400">
+              เทเลโฟโต้ ไวด์ และอัลตร้าไวด์
+            </p>
+          </div>
+          <div className="my-10">
+            <img
+              alt="Battery"
+              className="mx-auto"
+              src="assets/compareProduct/iphone/iphone-compare-icon-battery.jpg"
+            />
+            <p className="mt-4 font-semibold">
+              เล่นวิดีโอ นานสูงสุด 28 ชั่วโมง⁴
+            </p>
+          </div>
+          <div className="mt-10">
+            <img
+              alt="MagSafe"
+              className="mx-auto"
+              style={{width:'23px'}}
+              src="assets/compareProduct/iphone/iphone-compare-icon-iphone-13-pro-magsafe.jpg"
+            />
+            <p className="mt-4 font-semibold">สามารถใช้งานได้กับ</p>
+            <p className="font-semibold">อุปกรณ์เสริม MagSafe</p>
+          </div>
+        </div>
+
+        <div className="p-3 md:border-r">
+          <div>
+            <img
+              alt="iPhone 13"
+              className="mx-auto mb-5"
+              src="assets/compareProduct/iphone/iphone-compare-iphone-13.jpg"
+            />
+            <p className="text-sm text-red-600">แนะนำ</p>
+            <h6 className="text-2xl font-medium">iPhone 13</h6>
+            <p className="mt-2 text-lg font-medium">ขุมพลังครบเครื่อง</p>
+            <p className="mt-1 text-lg font-medium">เริ่มต้นที่ ฿25,900</p>
+            <img
+              alt="iPhone 13 Pro"
+              className="mx-auto mt-5"
+              src="assets/compareProduct/iphone/iphone-compare-iphone-13-swatch.jpg"
+            />
+          </div>
+          <div className="my-10">
+            <p className="text-2xl font-medium">6.1″ หรือ 5.4″</p>
+            <p className="mt-1 font-semibold">จอภาพ OLED ทั้งหน้าจอ¹</p>
+          </div>
+          <div className="my-10">
+            <img
+              alt="5G"
+              className="mx-auto"
+              src="assets/compareProduct/iphone/iphone-compare-icon-5g.jpg"
+            />
+            <p className="mt-4 font-semibold">5G ที่เร็วสุดแรง³</p>
+          </div>
+          <div className="my-10">
+            <img
+              alt="Chip"
+              className="mx-auto"
+              src="assets/compareProduct/iphone/iphone-compare-icon-a15-chip.jpg"
+            />
+            <p className="mt-4 font-semibold">ชิพ A15 Bionic</p>
+            <p className="font-semibold text-gray-400">
+              ชิปที่เร็วที่สุดในสมาร์ทโฟน
+            </p>
+          </div>
+          <div className="my-10">
+            <img
+              alt="Camera"
+              className="mx-auto"
+              src="assets/compareProduct/iphone/iphone-compare-icon-iphone-13-camera.jpg"
+            />
+            <p className="mt-4 font-semibold">ระบบกล้องคู่สุดล้ำ</p>
+            <p className="font-semibold text-gray-400">ไวด์ และอัลตร้าไวด์</p>
+          </div>
+          <div className="my-10">
+            <img
+              alt="Battery"
+              className="mx-auto"
+              src="assets/compareProduct/iphone/iphone-compare-icon-battery.jpg"
+            />
+            <p className="mt-4 font-semibold">
+              เล่นวิดีโอ นานสูงสุด 19 ชั่วโมง⁴
+            </p>
+          </div>
+          <div className="mt-10">
+            <img
+              alt="MagSafe"
+              className="mx-auto"
+              style={{width:'23px'}}
+              src="assets/compareProduct/iphone/iphone-compare-icon-iphone-13-magsafe.jpg"
+            />
+            <p className="mt-4 font-semibold">สามารถใช้งานได้กับ</p>
+            <p className="font-semibold">อุปกรณ์เสริม MagSafe</p>
+          </div>
+        </div>
+
+        <div className="p-3">
+          <div>
+            <img
+              alt="iPhone 12"
+              className="mx-auto mb-5"
+              src="assets/compareProduct/iphone/iphone-compare-iphone-12.jpg"
+            />
+            <p className="text-sm text-white">-</p>
+            <h6 className="text-2xl font-medium">iPhone 12</h6>
+            <p className="mt-2 text-lg font-medium">น่าทึ่งอย่างที่เคย</p>
+            <p className="mt-1 text-lg font-medium">เริ่มต้นที่ ฿21,900</p>
+            <img
+              alt="iPhone 13 Pro"
+              className="mx-auto mt-5"
+              src="assets/compareProduct/iphone/iphone-compare-iphone-12-swatch.jpg"
+            />
+          </div>
+          <div className="my-10">
+            <p className="text-2xl font-medium">6.1″ หรือ 5.4″</p>
+            <p className="mt-1 font-semibold">จอภาพ OLED ทั้งหน้าจอ¹</p>
+          </div>
+          <div className="my-10">
+            <img
+              alt="5G"
+              className="mx-auto"
+              src="assets/compareProduct/iphone/iphone-compare-icon-5g.jpg"
+            />
+            <p className="mt-4 font-semibold">5G ที่เร็วสุดแรง³</p>
+          </div>
+          <div className="my-10">
+            <img
+              alt="Chip"
+              className="mx-auto"
+              src="assets/compareProduct/iphone/iphone-compare-icon-a14-chip.jpg"
+            />
+            <p className="mt-4 font-semibold">ชิพ A14 Bionic</p>
+            <p className="font-semibold text-white">-</p>
+          </div>
+          <div className="my-10">
+            <img
+              alt="Camera"
+              className="mx-auto"
+              src="assets/compareProduct/iphone/iphone-compare-icon-iphone-12-camera.jpg"
+            />
+            <p className="mt-4 font-semibold">ระบบกล้องคู่</p>
+            <p className="font-semibold text-gray-400">ไวด์ และอัลตร้าไวด์</p>
+          </div>
+          <div className="my-10">
+            <img
+              alt="Battery"
+              className="mx-auto"
+              src="assets/compareProduct/iphone/iphone-compare-icon-battery.jpg"
+            />
+            <p className="mt-4 font-semibold">
+              เล่นวิดีโอ นานสูงสุด 17 ชั่วโมง⁴
+            </p>
+          </div>
+          <div className="mt-10">
+            <img
+              alt="MagSafe"
+              className="mx-auto"
+              style={{width:'23px'}}
+              src="assets/compareProduct/iphone/iphone-compare-icon-iphone-12-magsafe.jpg"
+            />
+            <p className="mt-4 font-semibold">สามารถใช้งานได้กับ</p>
+            <p className="font-semibold">อุปกรณ์เสริม MagSafe</p>
+          </div>
+        </div>
+      </div>
+      <p className="text-center text-lg font-medium mt-14">
+        <span
+          className="text-apple-link-blue hover:underline"
+        >
+          เปรียบเทียบ iPhone ทุกรุ่น
+          <KeyboardArrowRightIcon />
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default Iphone14ProCompare;
---
+++ src/pages/Iphone14Pro/Features.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import { textAlign } from '@mui/system';
+
+const Iphone14ProFeatures = () => {
+  return (
+    <div>
+      <h2 className="text-4xl font-semibold text-center mt-20">
+        โปรยิ่งกว่าโปร
+      </h2>
+      <div className="keyfeatures-container mt-10">
+        <div className="keyfeature-col keyfeature-col-span-4 grid grid-cols-2">
+          <div className="p-7">
+            <p className="text-3xl font-medium text-gray-500">
+              <span className="text-black">
+                ความละเอียดมากขึ้นสูงสุด 4 เท่า
+              </span>{' '}
+              iPhone 14 Pro สร้างมาตรฐานใหม่ให้กับสิ่งที่ 48 เมกะพิกเซลทำได้ นั่นคือการ
+              บันทึกภาพด้วยความละเอียดที่มากขึ้นถึง 4 เท่าในแบบ ProRAW แล้วคุณจะตื่นตาตื่นใจไปกับรายละเอียดในทุกการครอป
+            </p>
+          </div>
+          <div>
+            <img
+              style={{ height: '450px' }}
+              alt="Camera Feature"
+              className="w-full rounded-r-2xl"
+              src="assets/features/iphone14pro/iphone-14-pro-key-features-camera.png"
+            />
+          </div>
+        </div>
+
+        <div className="keyfeature-col keyfeature-col-span-2">
+          <div className="p-7">
+            <p className="text-3xl font-medium text-gray-500">
+              <span className="text-black">Dynamic Island</span>{' '}
+              นวัตกรรมของ Apple อย่างแท้จริง
+            </p>
+          </div>
+          <div className="absolute bottom-0">
+            <img
+              style={{ height: '350.5px'}}
+              alt="dynamic Feature"
+              className="h-full rounded-b-2xl"
+              src="assets/features/iphone14pro/iphone-14-pro-key-features-dynamic.png"
+            />
+          </div>
+        </div>
+
+        <div className="keyfeature-col keyfeature-col-span-3">
+          <div className="p-7 w-3/4">
+            <p className="text-3xl font-medium text-gray-500">
+              <span className="text-black">
+                จอภาพแบบติดตลอด พร้อมอยู่ตลอด
+              </span>{' '}
+              มาพร้อมกับแบตเตอรี่ที่ใช้งานได้ตลอดทั้งวัน
+            </p>
+          </div>
+          <div className="absolute bottom-0 right-0">
+            <img
+              alt="Display Feature"
+              src="assets/features/iphone14pro/iphone-14-pro-key-features-display.png"
+            />
+          </div>
+        </div>
+        <div className="keyfeature-col keyfeature-col-span-3">
+          <div className="w-full absolute" style={{ top: '-10px' }}>
+            <img
+              alt="Chip Feature"
+              className="mx-auto rounded-r-2xl"
+              style={{ maxWidth: '440px' }}
+              src="assets/features/iphone14pro/iphone-14-pro-key-features-chip.webp"
+            />
+          </div>
+          <div className="absolute bottom-0 p-7">
+            <p className="text-3xl text-center font-medium text-gray-500">
+              <span className="text-black">A16 Bionic</span>{' '}
+              ชิปสมาร์ทโฟนที่เร็วที่สุดในโลก
+              <br />
+              <span className="text-black">5G ที่เร็วสุดแรง</span>{' '}
+              เพื่อการดาวน์โหลดที่เร็วยิ่งขึ้น และการสตรีมคุณภาพสูง³
+            </p>
+          </div>
+        </div>
+        <div className="keyfeature-col keyfeature-col-span-4">
+          <div className="p-7">
+            <p className="text-3xl text-center font-medium text-gray-500">
+              <span className="text-black">
+                กล้องหลักความละเอียด 48MP พร้อมเซ็นเซอร์ แบบ Quad-pixel ที่ล้ำสมัย
+              </span>{' '}
+              ระบบกล้องระดับโปรที่ โปรอย่างยิ่งใหญ่กว่าครั้งไหนๆ
+            </p>
+          </div>
+          <div className="absolute bottom-0">
+            <img
+              style={{ height: '300px' }}
+              alt="camera Feature"
+              className="rounded-r-2xl"
+              src="assets/features/iphone14pro/iphone-14-camera-shaking.jpg"
+            />
+          </div>
+        </div>
+        <div className="keyfeature-col keyfeature-col-span-2">
+          <div className="p-7">
+            <p className="text-3xl font-medium text-gray-500">
+              <span className="text-black">Ceramic Shield</span>{' '}
+              แข็งแกร่งกว่ากระจกไหนๆ บนสมาร์ทโฟน ความสามารถในการทนน้ำ
+              เสริมด้วยสแตนเลสสตีลเกรดเดียวกับที่ใช้ทำเครื่องมือศัลยกรรม
+            </p>
+          </div>
+          <div className="absolute bottom-0 right-0">
+            <img
+              alt="Glass Feature"
+              className="h-full rounded-b-2xl"
+              style={{ maxWidth: '200px' }}
+              src="assets/features/iphone14pro/iphone-14-pro-key-features-glass.jpg"
+            />
+          </div>
+        </div>
+      </div>
+      <p className="text-center text-lg font-medium mt-14">
+        <span
+          className="text-apple-link-blue hover:underline"
+        >
+          ดูเพิ่มเติมเกี่ยวกับ iPhone 14 Pro
+          <KeyboardArrowRightIcon />
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default Iphone14ProFeatures;
