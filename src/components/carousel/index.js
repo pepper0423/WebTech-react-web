77b445532de75d2bc8b3f1fcda336c3e8ebbb9f4
Worrasit Makhasit <64070098@it.kmitl.ac.th>
2022-11-09 16:23:29 +0700

    change carousel vid

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -3,17 +3,17 @@ import { Link } from 'react-router-dom';
 
 function Carousel() {
     return (
-        <div className='max-w-full bg-gray-100'>
+        <div className='max-w-full bg-gray-900'>
             <div>
                 <video autoPlay loop width='80%' className='mx-auto brightness-75'>
-                    <source src="../assets/image/homepage/ipadcarouselvid1.mp4" type='video/mp4'/>
+                    <source src="../assets/image/homepage/iphonecarouselvid.mp4" type='video/mp4'/>
                 </video>
             </div>
             <div className='floatin'>
-                <h2><strong>iPad</strong></h2>
+                <h2><strong>iPhone 14 Pro</strong></h2>
                 <p>สุดสดใสแบบใหม่หมดสำหรับสิ่งต่างๆ ที่คุณทำทุกวัน</p>
-                <Link to="/ipadgen10" id='buy'>ซื้อ &#10095;</Link>
-                <Link to="/ipad" className="mr-4" id='seemore'>ดูเพิ่มเติม &#10095;</Link>
+                <Link to="/iphone14pro" id='buy'>ซื้อ &#10095;</Link>
+                <Link to="/iphone" className="mr-4" id='seemore'>ดูเพิ่มเติม &#10095;</Link>
             </div>
         </div>
     );
