4268d07962dc2b68b9bd5e82ef3d2946a685070d
Worrasit Makhasit <64070098@it.kmitl.ac.th>
2022-11-14 23:33:48 +0700

    ipad fixed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Foot from "./components/foot";
 import Iphone14Pro from './pages/Iphone14Pro';
 import Iphone14 from './pages/Iphone14';
 import Iphone from './pages/iphone';
+import Ipad from "./pages/Ipad";
+import Ipadgen10 from "./pages/ipadgen10";
 
 const App =()=> {
   return (
@@ -16,6 +18,8 @@ const App =()=> {
         <Route path="iphone14pro" element={<Iphone14Pro/>}/>
         <Route path="iphone14" element={<Iphone14/>}/>
         <Route path="iphone" element={<Iphone/>}/>
+        <Route path="ipad" element={<Ipad/>}/>
+        <Route path="ipadgen10" element={<Ipadgen10/>}/>
       </Routes>
       <Foot/>
     </div>
