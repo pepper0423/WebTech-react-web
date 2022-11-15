b1f1a7f246cccb1e3273227a71efac9556e9855e
Worrasit Makhasit <64070098@it.kmitl.ac.th>
2022-11-15 17:59:15 +0700

    bag path

--- src/components/nav/bagpop.js
+++ src/components/nav/bagpop.js
@@ -33,7 +33,7 @@ const Bag = () => {
                     <BagItems bag={bag} />
                     <AppleButton className='w-full'>ชำระเงิน</AppleButton>
                     <div className='my-3 divide-y divide-gray-300'>
-                        <Link to='bag'>
+                        <Link to='/bag'>
                             <div className='flex py-2 text-small text-apple-blue hover:underline'>
                                 <BsBag className='fill-apple-blue' />
                                 <span className='ml-2'>ถุง {!!bag ? `(${bag.length})` : '(0)'}</span>
